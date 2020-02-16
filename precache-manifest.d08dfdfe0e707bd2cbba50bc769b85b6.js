b1246addb3496b45365451001c7d0696aac50c02
koolii <koolii0909@gmail.com>
2020-02-16 19:08:24 +0100

    Updates

---
+++ precache-manifest.d08dfdfe0e707bd2cbba50bc769b85b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df53c54f4245cddb72a2686d647d508b",
+    "url": "/react-audio-recorder/index.html"
+  },
+  {
+    "revision": "04c536ad31f28c06ef56",
+    "url": "/react-audio-recorder/static/css/main.def1bdb0.chunk.css"
+  },
+  {
+    "revision": "a137c9d3cfd1ebeedb09",
+    "url": "/react-audio-recorder/static/js/2.e7fec8d4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-audio-recorder/static/js/2.e7fec8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c536ad31f28c06ef56",
+    "url": "/react-audio-recorder/static/js/main.20a5fe95.chunk.js"
+  },
+  {
+    "revision": "9b6d0bc34f70da774c0f",
+    "url": "/react-audio-recorder/static/js/runtime-main.58c27a9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-audio-recorder/static/media/logo.5d5d9eef.svg"
+  }
+]);
